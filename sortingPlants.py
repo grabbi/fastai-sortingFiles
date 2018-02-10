e8484463ed6ec585ad61f50213eff80c312f76ed
Niclas <nicgrabrs@gmx.de>
2018-02-10 13:36:04 +0100

    add sorting file for plants competition

--- sortingPlants.py
+++ sortingPlants.py
@@ -1,14 +1,12 @@
 import os, shutil, random
-import pandas as pd
-path = 'dogbreed'
+
+path = 'plants'
 trainPath = '../' + path +'/train/'
 testPath =  '../' + path +'/test/'
 
 shuffleSampleTrainN = 4
 shuffleSampleValidN = 2
 shuffleValidN = 20
-
-df = pd.read_csv('../' + path + '/labels.csv', sep=",")
 
 
 if os.path.exists(trainPath):
@@ -20,16 +18,6 @@ if os.path.exists(trainPath):
 
     if not os.path.exists('../' + path + '/valid'):
         os.makedirs('../' + path + '/valid')
-    
-    if not os.path.exists('../' + path + '/test/unknown'):
-        os.makedirs('../' + path + '/unknown')
-
-    for f in os.listdir(trainPath):
-        breed = df[df["id"] == str.split(f, '.')[0]]["breed"].values[0]
-        if not os.path.exists(trainPath + breed):
-            os.makedirs(trainPath + breed)
-        shutil.move(trainPath + f , trainPath + breed + '/' + f)
-    
 
     for f in os.listdir(trainPath):
         if not os.path.exists('../' + path + '/sample/train/' + f):
@@ -51,11 +39,6 @@ if os.path.exists(trainPath):
         samples = random.sample(os.listdir(trainPath + f), shuffleValidN)
         for s in samples:
             shutil.move(trainPath + f + '/' + s , '../' + path + '/valid/' + f + '/' + s)
-    
-    for f in os.listdir(testPath):
-        shutil.move(testPath + f , '../' + path + '/unknown/' + f)
-    
-    shutil.move('../' + path + '/unknown', '../' + path + '/test/')
 
     print("Finished sorting!")
 
